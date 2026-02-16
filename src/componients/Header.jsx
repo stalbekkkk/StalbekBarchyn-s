951bd92fed5fc73dd5422fa177b8f050c56f53d6
Stalbek <stalbekghingizov@gmail.com>
2026-02-16 23:27:50 +0600

    add header

---
+++ src/componients/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div>
+      <h1>Header</h1>git add .
+git commit -m "add components folder"
+git push
+
+    </div>
+  )
+}
+
+export default Header
